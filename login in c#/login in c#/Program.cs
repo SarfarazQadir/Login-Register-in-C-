d9180d587f0e680c81d195212e22e7cb56b29cc1
Sarfaraz Qadir <sarfarazqadir594@gmail.com>
2024-04-30 06:00:47 +0500

    Add files via upload

---
+++ login in c#/login in c#/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Login In C#");
+
+
+Console.WriteLine("Enter Your Username");
+string username = Console.ReadLine();
+
+
+
+Console.WriteLine("Enter Your Password");
+string Password = Console.ReadLine();
+
+
+if(username == "sarfarazqadir14" && Password == "neymarjr")
+{
+    Console.WriteLine("Login Successfull");
+}
+else
+{
+    Console.WriteLine("Login Failed!!");
+}
